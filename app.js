69df8b8598ac132a7c825cf88fc97b35e3710444
DanizGudrat <deniz.qudret16@mail.ru>
2024-08-12 22:37:08 +0400

    homework

---
+++ app.js
@@ -0,0 +1,88 @@
+// console.log("hello world");
+
+// console.log("salam dunya");
+
+// console.warn("internet zeifdir!");
+
+// console.error("internet islemir!");
+
+// console.clear();
+
+// console.log("salam");
+
+
+// console.log(25);
+
+// var age = 25;
+// console.log(age);
+
+// let age1 = 30;
+// console.log(age1);
+
+// const age2 = 35;
+// console.log(age2);
+
+
+// var data = 10;
+// var data = "Kanan";
+// console.log(data);
+
+// let data1 = 20;
+// let data1 = "Osman";
+// console.log(data1);
+
+
+// const data2 = 40;
+// const data2 = "Sanan";
+// console.log(data2);
+
+// let user = "Ali";
+// user = "Kamil";
+// console.log(user);
+
+// const player = "Osman";
+// player = "Anar";
+// console.log(player);
+
+
+
+// let name;
+// name = "Osman";
+// console.log(name);
+
+// const user = "Ali";
+// console.log(user);
+
+
+// const name = "Ali";
+// console.log(name);
+
+
+// let myFirstNewName
+
+// number
+
+// let a = 40;
+// let b = 10;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a / 5);
+
+
+// let num = -40.1234569;
+// console.log(num.toFixed(3));
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.round(num));
+// console.log(Math.abs(num));
+// console.log(Math.round(Math.random()));
+// console.log(Math.random() * 10);
+// console.log(Math.max(4,1,0,5,74,-45,69));
+// console.log(Math.min(4,1,0,5,74,-45,69));
+
+
+// let bookPrice = 10;
+// let bookAmount = 5;
+// console.log(bookPrice * bookAmount);
